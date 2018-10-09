a986d3b524fab14a77f093b69339f1081e6fa2a2
Kanat@Itch_PC <kanatkubash@gmail.com>
2018-10-09 12:03:51 +0600

    Change ru, en translations to use plain object

--- src/locale/translations/en.js
+++ src/locale/translations/en.js
@@ -1,10 +1,36 @@
-import Language from '../Language'
-
-export default new Language(
-  'English',
-  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-)
+export default {
+  language: "English",
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
+  monthsAbbr: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ],
+  days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  rtl: false,
+  ymd: false,
+  yearSuffix: ""
+};
 // eslint-disable-next-line
-;
